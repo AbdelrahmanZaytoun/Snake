26964d5ca8979a52404bce3c5c51e303394effe3
abdelrahman zaytoun <abdelrahmanzaytounn@gmail.com>
2021-08-30 19:01:16 -0400

    fixing bugs

--- client.js
+++ client.js
@@ -7,16 +7,10 @@ const connect = function () {
       host:IP,// IP address here,
       port: PORT // PORT number here,
     });
-  
     // interpret incoming data as text
     conn.setEncoding("utf8");
     conn.on("connect",()=>{console.log("Successfully connected to game server")})
     conn.on('connect', () => { conn.write(' Name: ZAY')});
-    conn.on('connect', () => { conn.write( "Move: down")});
-   
-  
-
-
     return conn;
   };
   
